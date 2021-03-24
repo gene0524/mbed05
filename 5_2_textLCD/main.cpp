bf1dd0efba202a6ec53d822b374af52d97661c19
gene_yu <yugene524@gmail.com>
2021-03-24 02:22:13 -0700

    add main.cpp

--- 5_2_textLCD/main.cpp
+++ 5_2_textLCD/main.cpp
@@ -38,8 +38,9 @@ int main()
 }
 */
 
-//Example 3
 
+//Example 3
+/*
 //Display ADC input data
 #include "mbed.h"
 #include "TextLCD.h"
@@ -62,5 +63,25 @@ int main()
             lcd.printf("%d", C);
             ThisThread::sleep_for(250ms);
             lcd.cls();
+      }
+}
+*/
+
+//Demo
+#include "mbed.h"
+#include "TextLCD.h"
+
+DigitalOut led(LED1);      //LED1 = PA_5
+TextLCD lcd(D2, D3, D4, D5, D6, D7);
+
+int main()
+{
+      lcd.printf("107030027\n");
+      for (int x=30; x>=0; --x)
+      {
+            led = !led;             // toggle led
+            lcd.locate(5,1);
+            lcd.printf("%5i",x);    //counter display
+            ThisThread::sleep_for(500ms);
       }
 }
--- 5_4_uLCD_demo/main.cpp
+++ 5_4_uLCD_demo/main.cpp
@@ -7,7 +7,7 @@ uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
 int main()
 {
-    // basic printf demo = 16 by 18 characters on screen
+    /*// basic printf demo = 16 by 18 characters on screen
     uLCD.printf("\nHello uLCD World\n"); //Default Green on black text
     uLCD.printf("\n  Starting Demo...");
     uLCD.text_width(4); //4X size text
@@ -17,5 +17,26 @@ int main()
         uLCD.locate(1,2);
         uLCD.printf("%2d",i);
         ThisThread::sleep_for(500ms);
+    }*/
+
+    uLCD.background_color(0xFFFFFF);
+    uLCD.textbackground_color(0xFFFFFF);
+    uLCD.cls();
+    uLCD.color(BLUE);
+    uLCD.printf("\nstudent ID\n");
+    uLCD.locate(0,3);
+    uLCD.text_width(2); //2X size text
+    uLCD.text_height(2);
+    uLCD.printf("107030027\n");
+
+    uLCD.text_width(4); //4X size text
+    uLCD.text_height(4);
+    uLCD.color(GREEN);
+    for (int i=30; i>=0; --i) 
+    {
+        uLCD.locate(2,2);
+        uLCD.printf("%2d",i);
+        ThisThread::sleep_for(500ms);
     }
+
 }
